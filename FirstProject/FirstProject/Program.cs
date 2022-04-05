6bb62011b385c4b21b797984f38a50d8daa77b1d
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-05 16:30:45 -0300

    FirstProject
    Criando program em C#

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
