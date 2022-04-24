56e5a618181e0f0c1969eb2a1e028f5653321761
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-24 13:12:25 -0300

    Program de mostrar raio e salario

---
+++ 1_Recapitulacao_Logica_Programao_CSharp/Exercicios/Exercicio 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 4 - Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+            //hora e calcula o salário desse funcionário.A seguir, mostre o número e o salário do funcionário, com duas casas
+            //decimais.
+            for (int i = 0; i < 3; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                int horas = Convert.ToInt32(Console.ReadLine());
+                double valorPorHora = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("NUMBER = " + number);
+                Console.WriteLine("SALARY = " + " U$ " + (horas * valorPorHora).ToString("0.00"));
+            }
+        }
+    }
+}
---
+++ 1_Recapitulacao_Logica_Programao_CSharp/Exercicios/Exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Exercicio 2 
+            //Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+            //casas decimais conforme exemplos.
+
+            double raio = Double.Parse(Console.ReadLine());
+            double area = 3.14159 * (Math.Pow(raio, 2));
+            Console.WriteLine(Math.Round(area, 4));
+        }
+    }
+}
