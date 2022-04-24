61a7552c21e90b8c530eaef29364e03ee5565589
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-24 13:11:25 -0300

    Numeros multiplos
    Exercicio de numeros multiplos ou nao

---
+++ 1_Recapitulacao_Logica_Programao_CSharp/2ListaEsxercicios/OperadoresComparativos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 % num2 == 0)
+                    Console.WriteLine("São multiplos");
+                else
+                    Console.WriteLine("Não são multiplos");
+            }
+            else
+            {
+                if (num2 % num1 == 0)
+                    Console.WriteLine("São multiplos");
+                else
+                    Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
