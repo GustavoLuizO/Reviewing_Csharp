c2e6cc3c21def7f6e79ccb4a5334cd895da8055f
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-24 13:11:45 -0300

    Delta e bhaskara

---
+++ 1_Recapitulacao_Logica_Programao_CSharp/Delta e Bhaskara/Delta e Bhaskara/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Delta_e_Bhaskara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+            Console.WriteLine("X1 = " + x1 + "\nX2 = " + x2);
+        }
+    }
+}
