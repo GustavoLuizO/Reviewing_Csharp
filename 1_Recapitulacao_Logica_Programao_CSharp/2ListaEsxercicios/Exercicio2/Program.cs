1125e0bc255b7c8da93718e142fd74be81ac4d8e
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-24 13:10:16 -0300

    Tempo que o jogo durou

---
+++ 1_Recapitulacao_Logica_Programao_CSharp/2ListaEsxercicios/Exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horainicial = int.Parse(Console.ReadLine());
+            int horafinal = int.Parse(Console.ReadLine());
+            if (horafinal <= horainicial)
+                Console.WriteLine("O JOGO DUROU  " + (24 - horainicial + horafinal));
+            else
+                Console.WriteLine("O jogo durou " + (horafinal - horainicial));
+
+    
+        }
+    }
+}
