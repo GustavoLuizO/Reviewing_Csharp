5a2cda1e74c1d2417644dbb1f6fcf6d9a3aa7959
Gustavo Luiz <glogustavo4321@gmail.com>
2022-06-28 16:04:55 -0300

    ContaBancariaProgramClass
    Programa referente a uma conta bancaria com metodos de saque e deposito.

---
+++ LastExercise-ContaBancaria/LastExercise-ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastExercise_ContaBancaria
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta {get ; private set;}
+        public string TitularConta { get ; private set;}
+        public double Saldo { get; private set;}
+
+        public ContaBancaria(int numeroConta, string titularConta)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta atualizados:\nConta " + NumeroConta + ", Titular: " + TitularConta + ", Saldo: $ " + Saldo.ToString("0.00");
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque (double valor)
+        {
+            double taxa = 5;
+            double valorTotalRetiradoSaldo = valor + taxa;
+            Saldo -= valorTotalRetiradoSaldo;
+        }
+    }
+}
---
+++ LastExercise-ContaBancaria/LastExercise-ContaBancaria/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LastExercise_ContaBancaria
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Entre o número da conta:");
+            int numeroConta = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Entre o titular da conta:");
+            string titularConta = Console.ReadLine();
+            ContaBancaria contaBancaria = new ContaBancaria(numeroConta, titularConta);
+            Console.Write("Haverá depósito inicial (s/n)?");
+            string opDepositoInicial = Console.ReadLine();
+            if (opDepositoInicial == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial : ");
+                double depositoInicial = Convert.ToDouble(Console.ReadLine());
+                contaBancaria.Deposito(depositoInicial);
+            }
+            Console.WriteLine("Dados da conta:\nConta " + contaBancaria.NumeroConta + ", Titular: " + contaBancaria.TitularConta + ", Saldo: $ " + contaBancaria.Saldo.ToString("0.00"));
+
+            Console.Write("Entre um valor para depósito:");
+            contaBancaria.Deposito(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(contaBancaria.ToString());
+            
+            Console.Write("Entre um valor para saque:");
+            contaBancaria.Saque(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(contaBancaria.ToString());
+        }
+    }
+}
