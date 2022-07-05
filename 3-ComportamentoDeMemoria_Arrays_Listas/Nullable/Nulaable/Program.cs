3f93d57cb65b3e6c80886d9b7e0d47d2a081965b
Gustavo Luiz <glogustavo4321@gmail.com>
2022-07-05 13:36:13 -0300

    HasValue, Value e GetValueOrDefault

---
+++ 3-ComportamentoDeMemoria_Arrays_Listas/Nullable/Nulaable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nulaable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? valor = null;
+            double? valor2 = 5.0;
+
+            //Return Valor ou o valor atribuido ao tipo dele
+            Console.WriteLine(valor.GetValueOrDefault());
+            Console.WriteLine(valor2.GetValueOrDefault());
+
+            //Tem valor ? 
+            Console.WriteLine(valor.HasValue);
+            Console.WriteLine(valor2.HasValue);
+
+
+            if (valor.HasValue)
+                Console.WriteLine(valor.Value);//Valor
+            
+            if (valor2.HasValue)
+                Console.WriteLine(valor2.Value);
+        }
+    }
+}
