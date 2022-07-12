4cc8a8688cd4273d9dfb88b5df19473872f3702d
Gustavo Luiz <glogustavo4321@gmail.com>
2022-07-12 11:21:49 -0300

    Hospedagem Ex

---
+++ 3-ComportamentoDeMemoria_Arrays_Listas/Vetores/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistroHospedagem[] quartosRegistro = new RegistroHospedagem[10];
+            
+            Console.WriteLine("Quantos quartos serão alugados ?");
+            int qtdQuartos = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < qtdQuartos; i++)
+            {
+                string nomeHospede = Console.ReadLine();
+                string emailHospede = Console.ReadLine();
+                int quartoEscolhido = Convert.ToInt32(Console.ReadLine());
+
+                quartosRegistro[quartoEscolhido] = new RegistroHospedagem { NameHospede = nomeHospede, EmailHospede = emailHospede };
+            }
+            
+            for (int i = 0; i < quartosRegistro.Length; i++)
+            {
+                if (quartosRegistro[i] != null)
+                {
+                    Console.WriteLine(i + ": " + quartosRegistro[i].NameHospede + " , " + quartosRegistro[i].EmailHospede);
+                }
+            }
+        }
+    }
+}
---
+++ 3-ComportamentoDeMemoria_Arrays_Listas/Vetores/Vetores/RegistroHospedagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores
+{
+    public class RegistroHospedagem
+    {
+        public string NameHospede { get; set; }
+        public string EmailHospede { get; set; }
+    }
+}
