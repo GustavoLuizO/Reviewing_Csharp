de4b010c6781f9f7151bafb5aa83db09a235338e
Gustavo Luiz <glogustavo4321@gmail.com>
2022-07-12 11:22:10 -0300

    Matrizes

---
+++ 3-ComportamentoDeMemoria_Arrays_Listas/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("How many places does the vector has ? ");
+            int qtdVec = Convert.ToInt32(Console.ReadLine());
+            int qtdNegativeValue = 0; 
+
+            int [,]vect = new int[qtdVec, qtdVec];
+            for (int i = 0; i < qtdVec; i++)
+            {
+                string []valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < qtdVec; j++)
+                {
+                    if(int.Parse(valores[j]) < 0)
+                        qtdNegativeValue++;
+                    vect[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.Write("Diagonal : ");
+            for (int i = 0; i < qtdVec; i++)
+            {
+                for (int j = 0; j < qtdVec; j++)
+                {
+                    if(i == j)
+                        Console.Write(vect[i, j] + " ");
+                }
+            }
+            Console.WriteLine("\nThe amount negative value : " + qtdNegativeValue);
+        }
+    }
+}
