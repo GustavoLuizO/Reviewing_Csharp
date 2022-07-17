0a5ea85fafcbd289207d21ff27988ce4df5bc0df
Gustavo Luiz <glogustavo4321@gmail.com>
2022-07-16 22:27:37 -0300

    Matrix

---
+++ 3-ComportamentoDeMemoria_Arrays_Listas/Matrizes/Matrizes_Fixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Matrizes_Fixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many stuff does the vector has ? ");
+        }
+    }
+}
