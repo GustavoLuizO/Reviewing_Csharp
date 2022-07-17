4d8caba953035adb47733069759f852fb96a3b89
Gustavo Luiz <glogustavo4321@gmail.com>
2022-07-16 23:01:14 -0300

    Interesting Function For String

---
+++ 4-Topicos_especiais_CSharp/Interesting_Functions_For_String/Interesting_Functions_For_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Interesting_Functions_For_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "  I am you !             ";
+            Console.WriteLine(phrase);
+
+            //Remove spaces at the beginning and end of the sentence
+            Console.WriteLine(phrase.Trim());
+
+            //Exchange something for something
+            Console.WriteLine(phrase.Replace("I", "YOU"));
+        }
+    }
+}
