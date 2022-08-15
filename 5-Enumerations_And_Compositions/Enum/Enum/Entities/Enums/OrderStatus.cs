3eadd46576429eb3c4d426f35582cfdba7239ac2
Gustavo Luiz <glogustavo4321@gmail.com>
2022-08-15 15:55:01 -0300

    Project learning enum in C#

---
+++ 5-Enumerations_And_Compositions/Enum/Enum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enum.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ 5-Enumerations_And_Compositions/Enum/Enum/Program.cs
@@ -0,0 +1,21 @@
+﻿using Enum.Entities;
+using Enum.Entities.Enums;
+using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order ordem = new Order()
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(ordem);
+        }
+    }
+}
