6beff41308bd87e907bdd1d5ad7f840e29ce36ae
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-06 15:57:39 -0300

    Create Program.cs

---
+++ 6-Heranca_Polimorfismo/Product/Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Product.Entities;
+
+namespace Product
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportedProduct importedProduct = new ImportedProduct(customsFree: 20.00, name: "Tablet", price: 260.00);
+            ProductCommom product = new ProductCommom(name: "Notebook", price:1100.00);
+            UsedProduct usedProduct = new UsedProduct(manufactureDate: DateTime.Now, name: "Iphone", price: 400.00);
+
+            Console.WriteLine(product.priceTag());
+            Console.WriteLine(usedProduct.priceTag());
+            Console.WriteLine(importedProduct.priceTag());
+        }
+    }
+}
