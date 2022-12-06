74b4d5077bf238bf3f3d13ecf2468b06b716aaf9
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-06 15:59:28 -0300

    Create WorkerLevel.cs

---
+++ 5-Enumerations_And_Compositions/Enum/Exercise/Class/ENUMS/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.ENUMS
+{
+    public enum WorkerLevel
+    {
+        Junior,
+        Pleno,
+        Senior
+    }
+}
