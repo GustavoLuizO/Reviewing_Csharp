400712cc8395cea7df5ddc46e9a26c51ff5a38f8
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-14 20:14:52 -0300

    Create ExceptionProject

---
+++ 7-Using Excepptions - Creat Exception/CreateException/CreateException/Acount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateException
+{
+    public class Acount
+    {
+        private int Number { get; set; }
+        private string Holder { get; set; }
+        private double Balance { get; set; }
+        private double WithdrawLimit { get; set; }
+
+        public Acount(int number, string holder, double initialBalance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = initialBalance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+            else if (amount > Balance)
+                throw new DomainException("Withdraw error: Not enough balance");
+            Balance -= amount;
+        }
+
+        public double ConsultBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ 7-Using Excepptions - Creat Exception/CreateException/CreateException/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CreateException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Acount acount = new Acount(8021, "Gustavo", 500.00, 600.00);
+
+                Console.Write("Enter amount for withdraw : ");
+                acount.WithDraw(Convert.ToDouble(Console.ReadLine()));
+
+                Console.WriteLine("New balance : " + acount.ConsultBalance());
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
