4b549d226e540b922252fa2bd8ec8e4d2b39a103
Gustavo Luiz <glogustavo4321@gmail.com>
2023-03-10 22:28:26 -0300

    - Utilizando o using para fechar automaticamente sem precisar do close

---
+++ 8-Using/Using/Using/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\gusta\Documents\GitHubRepositorios\Reviewing_Csharp\8-Using\Using\Using\text.txt";
+
+            try
+            {
+                //Ao final da execução, o using fecha os argumentos que estão referenciados.
+                //Ou seja, o fs e o sr já são fechado, não precisando dar o close em ambos.
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                            Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            }
+            catch (IOException erro)
+            {
+                Console.WriteLine("An error occurred. " + erro.Message);
+            }
+
+        }
+    }
+}
