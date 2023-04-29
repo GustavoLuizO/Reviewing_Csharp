bd1c7d5e08fb5992a0d2c4c9d4d4d0e5d922338b
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2023-04-28 23:01:50 -0300

    Create a project generics using typesafe

---
+++ 9-Generics-Set-Dictionary/Generics/Generics/PrintService.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics
+{
+    public class PrintService<T>
+    {
+        private T[] _values = new T[3];
+        private int _count = 0;
+
+        public void AddValeu(T value)
+        {
+            if (_count == 3)
+                throw new InvalidOperationException("PrintService is full");
+
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            foreach (T value in _values) 
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ 9-Generics-Set-Dictionary/Generics/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printServiceInteger = new PrintService<int>();
+
+            printServiceInteger.AddValeu(1);
+            printServiceInteger.AddValeu(2);
+            printServiceInteger.AddValeu(3);
+
+            Console.WriteLine("Primeiro valor : {0}", printServiceInteger.First());
+            Console.WriteLine("Valores : ");
+            printServiceInteger.Print();
+
+
+            Console.WriteLine();
+            PrintService<string> printServiceString = new PrintService<string>();
+            printServiceString.AddValeu("Americo");
+            printServiceString.AddValeu("Henrique");
+            printServiceString.AddValeu("Lucio");
+
+            Console.WriteLine("Primeiro valor : {0}", printServiceString.First());
+            Console.WriteLine("Valores : ");
+            printServiceString.Print();
+        }
+    }
+}
