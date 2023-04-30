3b5c94b13943597244e13d6abaffced25e0b42d1
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2023-04-30 20:44:24 -0300

    Using Action with forEach

---
+++ 10-ExpressoesLambda-Delegates-Linq/Comparison/ActionWithForeach/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ActionWithForeach
+{
+    public class Product
+    {
+        public string Nome { get; set; }
+        public double Priece { get; set; }
+
+        public Product(string nome, double priece)
+        {
+            Nome = nome;
+            Priece = priece;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " | " + "Priece: " + Priece;
+        }
+    }
+}
---
+++ 10-ExpressoesLambda-Delegates-Linq/Comparison/ActionWithForeach/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ActionWithForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("America", 450.00));
+
+            products.ForEach(UpdatePriece);
+
+            foreach (Product product in products)
+                Console.WriteLine(product);
+            
+        }
+        
+        static void UpdatePriece(Product product)
+        {
+            product.Priece += product.Priece * 0.1;
+        }
+    }
+}
