3a2ff023d0775f00f6bab401a2615bc5d19a4d83
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2023-04-30 20:22:06 -0300

    Comparison

---
+++ 10-ExpressoesLambda-Delegates-Linq/Comparison/Comparison/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace Comparison
+{
+    public class Product
+    {
+        public string Nome { get; }
+        public float Priece { get; }
+
+        public Product(string nome, float priece)
+        {
+            Nome = nome;
+            Priece = priece;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " | " + "Priece: " + Priece;
+        }
+    }
+}
---
+++ 10-ExpressoesLambda-Delegates-Linq/Comparison/Comparison/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900));
+            products.Add(new Product("Notebook", 1200));
+            products.Add(new Product("America", 450));
+
+            //products.Sort(CompareProductsForName);
+            products.Sort(CompareProductsForPriece);
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        static int CompareProductsForName(Product product1, Product product2)
+        {
+            return product1.Nome.ToUpper().CompareTo(product2.Nome.ToUpper());
+        }
+
+        static int CompareProductsForPriece(Product product1, Product product2)
+        {
+            return product1.Priece.CompareTo(product2.Priece);
+        }
+    }
+}
